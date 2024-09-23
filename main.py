ecce94671df26d3fc91dc732232bc094f9f0e6ee
Sohei.Suzuki <suzuki.s1008@gmail.com>
2024-09-23 19:25:06 +0900

    feat: termux-dialog

--- main.py
+++ main.py
@@ -1,25 +1,82 @@
+#!/usr/bin/env python
 import json
 from json import JSONDecodeError
 import subprocess
+from typing import Any
 
-if __name__ == "__main__":
-    command = [
-        "termux-dialog",
-        "radio",
-        "-t 'form' -v 'a,b,c'",
-    ]
-    command = ["termux-dialog"]
+TITLE = "家計簿"
+
+
+def exec_command(command: list) -> Any:
+    """
+    method for execution util
+    """
     res = subprocess.run(
         command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
     )
     json_str = res.stdout.decode("utf-8")
-    # print(json_str)
 
     try:
         data = json.loads(json_str)
     except JSONDecodeError as e:
         raise e
+    return data
 
-    print("code:", data["code"])
-    print("text:", data["text"])
-    print("index:", data["index"])
+
+def select_expense_type() -> str:
+    """
+    select expense type
+    """
+    data = exec_command(
+        [
+            "termux-dialog",
+            "sheet",
+            "-t",
+            TITLE,
+            "-v",
+            "食費,交通費,遊興費,雑費,書籍費,医療費",
+        ]
+    )
+    expense_type = str(data["text"])
+    print("expense_type: ", expense_type)
+    return expense_type
+
+
+def enter_expense_amount(expense_type: str) -> int:
+    """
+    enter expense amount
+    """
+    data = exec_command(
+        [
+            "termux-dialog",
+            "text",
+            "-t",
+            TITLE,
+            "-i",
+            f"Amount for {expense_type}",
+            "-n",
+        ]
+    )
+    expense_amount = int(data["text"])
+    print("expense_amount: ", expense_amount)
+    return expense_amount
+
+
+def main() -> None:
+    """ """
+    expense_type = select_expense_type()
+    expense_amount = enter_expense_amount(expense_type)
+    exec_command(
+        [
+            "termux-dialog",
+            "confirm",
+            "-t",
+            TITLE,
+            "-i",
+            f"expense_type: {expense_type}, expense_amount: {expense_amount}",
+        ]
+    )
+
+
+if __name__ == "__main__":
+    main()
