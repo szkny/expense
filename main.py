7b29c89fea0a06315115dcf678da602c1626a909
Sohei.Suzuki <suzuki.s1008@gmail.com>
2025-05-19 00:08:48 +0900

    feat: 🎸 register recent item

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 家計簿スプレッドシートに自動で書き込みを行うバッチプログラム
 """
 import os
+import re
 import json
 import asyncio
 import argparse
@@ -17,6 +18,7 @@ TITLE = "家計簿"
 
 HOME = os.getenv("HOME") or "~"
 os.makedirs(HOME + "/tmp/expense", exist_ok=True)
+EXPENSE_HISTORY = HOME + "/tmp/expense/expense_history.log"
 
 log.basicConfig(
     level=log.DEBUG,
@@ -48,11 +50,18 @@ async def main(args: argparse.Namespace) -> None:
                 f"🗓️{today_str}\n{todays_expenses}",
             )
         else:
-            expense_type = select_expense_type()
-            expense_amount = enter_expense_amount(expense_type)
-            expense_memo = enter_expense_memo(
-                f"{expense_type}(¥{expense_amount:,})"
-            )
+            recent_expenses = load_history(3)
+            expense_type = select_expense_type(recent_expenses)
+            if "🕒️" in expense_type:
+                data = expense_type.replace("🕒️ ", "").split(":")
+                expense_type = data[0]
+                expense_memo = data[1]
+                expense_amount = int(re.sub(r"[^\d]", "", data[2]))
+            else:
+                expense_amount = enter_expense_amount(expense_type)
+                expense_memo = enter_expense_memo(
+                    f"{expense_type}(¥{expense_amount:,})"
+                )
             # res = confirmation(
             #     f"以下の内容で登録しますか？\n\t{expense_type}{':'+expense_memo if expense_memo else ''}, ¥{expense_amount:,}"
             # )
@@ -61,6 +70,7 @@ async def main(args: argparse.Namespace) -> None:
             loop.run_in_executor(None, lambda: toast("登録中.."))
             handler = GspreadHandler(bookname)
             handler.register_expense(expense_type, expense_amount, expense_memo)
+            store_expense(expense_type, expense_memo, expense_amount)
             notify(
                 "家計簿への登録が完了しました。",
                 f"{expense_type}{':'+expense_memo if expense_memo else ''}, ¥{expense_amount:,}",
@@ -70,6 +80,57 @@ async def main(args: argparse.Namespace) -> None:
         notify("🚫家計簿の登録処理に失敗しました。", str(e))
     finally:
         log.info("end 'main' method")
+
+
+def load_history(num_items: int = 3) -> list:
+    """
+    load history
+    """
+    log.info("start 'load_history' method")
+    if not os.path.exists(EXPENSE_HISTORY):
+        return []
+
+    def parse_row(row: str) -> dict:
+        data = row.strip().split(",")
+        if len(data) == 2:
+            return {
+                "expense_type": data[0],
+                "expense_memo": "",
+                "expense_amount": int(data[1]),
+            }
+        elif len(data) == 3:
+            return {
+                "expense_type": data[0],
+                "expense_memo": data[1],
+                "expense_amount": int(data[2]),
+            }
+        else:
+            return {}
+
+    with open(EXPENSE_HISTORY, "r") as f:
+        lines = f.readlines()[::-1]
+    lines = [",".join(line.split(",")[1:]) for line in lines]
+    print(lines)
+    lines = list(dict.fromkeys(lines))  # remove duplicates
+    print(lines)
+    recent_expenses: list[dict] = [parse_row(row) for row in lines[:num_items]]
+    log.info("end 'load_history' method")
+    return recent_expenses
+
+
+def store_expense(
+    expense_type: str, expense_memo: str, expense_amount: int
+) -> None:
+    """
+    store expense
+    """
+    log.info("start 'store_expense' method")
+    with open(EXPENSE_HISTORY, "a") as f:
+        f.write(
+            f"{datetime.datetime.today().isoformat()},{expense_type},{expense_memo},{expense_amount}\n"
+        )
+    log.info("end 'store_expense' method")
+    return
 
 
 def get_fiscal_year() -> int:
@@ -115,11 +176,20 @@ def exec_command(command: list, timeout: int = 60) -> Any:
     return data
 
 
-def select_expense_type() -> str:
+def select_expense_type(recent_items: list[dict] = []) -> str:
     """
     select expense type
     """
     log.info("start 'select_expense_type' method")
+    items_list_str = "食費,交通費,遊興費,雑費,書籍費,医療費,家賃,光熱費,通信費,養育費,特別経費,給与,雑所得"
+    if len(recent_items):
+        recent_items_str = ",".join(
+            [
+                f'🕒️ {i["expense_type"]}:{i["expense_memo"]}:¥{i["expense_amount"]}'
+                for i in recent_items
+            ]
+        )
+        items_list_str = items_list_str + "," + recent_items_str
     data = exec_command(
         [
             "termux-dialog",
@@ -127,7 +197,7 @@ def select_expense_type() -> str:
             "-t",
             TITLE,
             "-v",
-            "食費,交通費,遊興費,雑費,書籍費,医療費,家賃,光熱費,通信費,養育費,特別経費,給与,雑所得",
+            items_list_str,
         ]
     )
     expense_type = str(data["text"])
